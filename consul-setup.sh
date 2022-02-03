0ac0f90944903ac5fc4d44c7bad43e836e248515
melwinvinod <54060915+melwinvinod@users.noreply.github.com>
2022-02-04 00:14:15 +0530

    Adding script to setup consul
    Docs: https://www.consul.io/downloads

---
+++ consul-setup.sh
@@ -0,0 +1,26 @@
+sudo apt install -y unzip
+sudo wget https://releases.hashicorp.com/consul/1.11.2/consul_1.11.2_linux_amd64.zip
+unzip consul_1.11.2_linux_amd64.zip
+sudo rm consul_1.11.2_linux_amd64.zip
+sudo cp consul /usr/bin
+
+{
+sudo tee -a  /etc/systemd/system/consul.service > /dev/null <<EOT
+[Unit]
+Description=Consul Server
+Documentation=https://www.consul.io/docs
+After=network.target
+
+[Service]
+ExecStart=/usr/sbin/sshd -D $SSHD_OPTS
+Restart=always
+RestartSec=1
+
+[Install]
+WantedBy=multi-user.target
+EOT
+}
+
+sudo systemctl daemon-reload
+sudo systemctl start consul.service
+sudo systemctl enable consul.service
