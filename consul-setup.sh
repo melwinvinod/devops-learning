c553c3b19c8a64e457b688135f0d46557982d543
melwinvinod <54060915+melwinvinod@users.noreply.github.com>
2022-02-04 00:27:39 +0530

    minor changes

--- consul-setup.sh
+++ consul-setup.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-
-sudo apt install -y unzip
-sudo wget https://releases.hashicorp.com/consul/1.11.2/consul_1.11.2_linux_amd64.zip
+sudo -i
+cd /opt
+apt install -y unzip
+wget https://releases.hashicorp.com/consul/1.11.2/consul_1.11.2_linux_amd64.zip
 unzip consul_1.11.2_linux_amd64.zip
-sudo rm consul_1.11.2_linux_amd64.zip
-sudo cp consul /usr/bin
+rm consul_1.11.2_linux_amd64.zip
+cp consul /usr/bin
 
 {
-sudo tee -a  /etc/systemd/system/consul.service > /dev/null <<EOT
+tee -a  /etc/systemd/system/consul.service > /dev/null <<EOT
 [Unit]
 Description=Consul Server
 Documentation=https://www.consul.io/docs
@@ -23,6 +24,6 @@ WantedBy=multi-user.target
 EOT
 }
 
-sudo systemctl daemon-reload
-sudo systemctl start consul.service
-sudo systemctl enable consul.service
+systemctl daemon-reload
+systemctl start consul.service
+systemctl enable consul.service
